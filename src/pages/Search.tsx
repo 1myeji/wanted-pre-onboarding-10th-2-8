2938f8177930434883a0e6116da35ca8d1ebeab2
imyeji <yimyg0596@naver.com>
2023-05-03 01:35:29 +0900

    Feat: SearchIcon 컴포넌트 분리 svg 아이콘 코드가 중복되어서

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,6 +1,7 @@
 import { useRef, useState } from 'react';
 import styled from 'styled-components';
 import { IsFocusedProps } from '../types/global';
+import SearchIcon from '../components/SearchIcon';
 
 const Search = () => {
   const [isFocused, setIsFocused] = useState(false);
@@ -20,14 +21,7 @@ const Search = () => {
       <SearchInputWrapper>
         <InputSection onClick={handleInputFocus}>
           <SearchIconWrapper isFocused={isFocused}>
-            <svg
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              preserveAspectRatio="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M6.56 0a6.56 6.56 0 015.255 10.49L16 14.674 14.675 16l-4.186-4.184A6.56 6.56 0 116.561 0zm0 1.875a4.686 4.686 0 100 9.372 4.686 4.686 0 000-9.372z"></path>
-            </svg>
+            <SearchIcon />
           </SearchIconWrapper>
           <PlaceholderText isFocused={isFocused}>질환명을 입력해 주세요.</PlaceholderText>
           <SearchInput
@@ -38,14 +32,7 @@ const Search = () => {
           />
         </InputSection>
         <SearchBtn>
-          <svg
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            preserveAspectRatio="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M6.56 0a6.56 6.56 0 015.255 10.49L16 14.674 14.675 16l-4.186-4.184A6.56 6.56 0 116.561 0zm0 1.875a4.686 4.686 0 100 9.372 4.686 4.686 0 000-9.372z"></path>
-          </svg>
+          <SearchIcon />
         </SearchBtn>
       </SearchInputWrapper>
     </MainContainer>
